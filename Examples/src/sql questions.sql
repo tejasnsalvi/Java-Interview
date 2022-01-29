c89c4c927502ea0d5c74bb10739121851c01b9a2
Tejas Nitin Salvi <36071254+tejasnsalvi@users.noreply.github.com>
2022-01-29 11:28:25 +0530

    g

--- Examples/src/sql questions.sql
+++ Examples/src/sql questions.sql
@@ -6,6 +6,16 @@ select * from (select salary from employee order by salary desc) t limit 1,1;
 
 /*Find 3rd highest salary*/
  limit 2,1; // LIMIT OFFSET,NO_OF_ROWS  offset 2 thats is leave two rows 
+ 
+ /*
+ ORACLE
+ 
+ SELECT fieldA,fieldB 
+FROM table 
+ORDER BY fieldA 
+OFFSET 5 ROWS FETCH NEXT 14 ROWS ONLY;
+ 
+ */
 
 /*Join three tables*/
 select e.id,e.name,d.id,d.dept_name,c.id,c.c_name 
